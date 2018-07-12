29f3022f2687ca62639f1f582bc1e0caec98ce15
MihailTS <tseluiko.m@gmail.com>
2018-07-12 04:37:29 +0300

    bsa-2018-php-8

---
+++ app/Currency.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    protected $table = 'currency';
+
+    protected $fillable = [
+        'title',
+        'short_name',
+        'logo_url',
+        'price',
+    ];
+}
